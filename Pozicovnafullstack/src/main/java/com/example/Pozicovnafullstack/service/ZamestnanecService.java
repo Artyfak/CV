168855f8636703e90a19959237c88f94f87063e1
Artyfak <112272916+Artyfak@users.noreply.github.com>
2022-10-06 10:52:00 +0200

    Delete Pozicovnafullstack directory

--- Pozicovnafullstack/src/main/java/com/example/Pozicovnafullstack/service/ZamestnanecService.java
+++
@@ -1,8 +0,0 @@
-package com.example.Pozicovnafullstack.service;
-
-
-import com.example.Pozicovnafullstack.model.Zamestnanec;
-
-public interface ZamestnanecService {
-    public Zamestnanec savezZamestnanec(Zamestnanec zamestnanec);
-}
